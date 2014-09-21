20c012411a4236bcd6a2890282725ecf565de99a
Frederick Wagner <doctaphred@gmail.com>
2014-09-21 13:42:43 -0400

    Added full quiz.

--- learn/views.py
+++ learn/views.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from datetime import datetime
 
 from django import forms
@@ -15,6 +16,74 @@ from .models import Token
 #     # TODO: get user's prescribed video, or a default.
 #     return render(request, 'learn.html', dictionary={'video': Video.objects.all()[0]})
 
+Question = namedtuple("Question", ['prompt', 'choices', 'answer'])
+
+quiz = [
+    Question(
+        "When an individual has incentives that work against their professional responsibilities, this is often called a _______.",
+        [
+            "Free Market",
+            "Fundamental Error",
+            "Conflict of Interest     x",
+            "System Failure",
+        ],
+        2
+    ),
+
+    Question(
+        "True or False: Conflicts of interest can be found frequently in society",
+        [
+            "True          x",
+            "False",
+        ],
+        0
+    ),
+
+    Question(
+        "When a doctor is seeking the right treatment for a patient, which of these factors would most likely become a conflict of interest?",
+        [
+            "Patient's medical history",
+            "A medicine company sending a doctor on a nice business trip          x",
+            "Doctor's salary increasing with years in the business",
+            "Patient grew up in the same state as the doctor",
+        ],
+        1
+    ),
+
+    Question(
+        "Which of these is a clear conflict of interest when someone is buying a home?",
+        [
+            "The real estate agent represents both the buyer and the seller            x",
+            "The buyers know the person selling the house personally",
+            "Two different real estate agents represent the buyer and the seller",
+            "The real estate agent for the buyer is also on the local school committee",
+        ],
+        0
+    ),
+
+    Question(
+        "Which professor is likely to design the least challenging class? A professor promoted on..",
+        [
+            "the content of the syllabus",
+            "the opinions of the faculty",
+            "the knowledge of their field",
+            "the opinions of the students     x",
+        ],
+        3
+    ),
+
+    Question(
+        "Which of these helps to reduce conflicts of interest?",
+        [
+            "Codes of Conduct",
+            "Respect customers or clients perspective",
+            "Consulting more-experienced or trustworthy people",
+            "All of the Above      x",
+        ],
+        4
+    ),
+]
+
 
 # TODO: multiple quiz forms, or random subset of questions.
 class QuizForm(forms.Form):
@@ -27,16 +96,58 @@ class QuizForm(forms.Form):
     watched_video = forms.ChoiceField(
         choices=WATCHED_VIDEO_CHOICES,
         label="Did you watch the video? (No lying.)",
-        # widget=forms.RadioSelect,
+        widget=forms.RadioSelect,
     )
 
-    code = forms.CharField(max_length=5)
+    # Unsure how dynamically monkey-patching these fields on would work with
+    # the Form class' declarative syntax, so explicitly declaring all questions for now.
+    q0 = forms.ChoiceField(
+        widget=forms.RadioSelect,
+        label=quiz[0].prompt,
+        choices=list(enumerate(quiz[0].choices))
+    )
+
+    q1 = forms.ChoiceField(
+        widget=forms.RadioSelect,
+        label=quiz[1].prompt,
+        choices=list(enumerate(quiz[1].choices))
+    )
+
+    q2 = forms.ChoiceField(
+        widget=forms.RadioSelect,
+        label=quiz[2].prompt,
+        choices=list(enumerate(quiz[2].choices))
+    )
+
+    q3 = forms.ChoiceField(
+        widget=forms.RadioSelect,
+        label=quiz[3].prompt,
+        choices=list(enumerate(quiz[3].choices))
+    )
+
+    q4 = forms.ChoiceField(
+        widget=forms.RadioSelect,
+        label=quiz[4].prompt,
+        choices=list(enumerate(quiz[4].choices))
+    )
+
+    q5 = forms.ChoiceField(
+        widget=forms.RadioSelect,
+        label=quiz[5].prompt,
+        choices=list(enumerate(quiz[5].choices))
+    )
+
+    redemption_code = forms.CharField(
+        max_length=5,
+        label='Enter your redemption code',
+    )
 
     def clean_watched_video(self):
         response = self.cleaned_data['watched_video']
         if response != 'yes':
             raise forms.ValidationError('Wrong answer!')
         return response
+
 
     def clean_code(self):
         code = self.cleaned_data['code']
@@ -48,6 +159,13 @@ class QuizForm(forms.Form):
                 raise forms.ValidationError('This code has already been used.')
         else:
             raise forms.ValidationError('This code is invalid.')
+
+
+    def clean(self):
+        for i, question in enumerate(quiz):
+            if self.cleaned_data['q{}'.format(i)] != question.answer:  # dynamic hax!
+                raise forms.ValidationError('Sorry, at least one quiz answer is incorrect.')
+        return self.cleaned_data
 
 
 # class CodeRedeemForm(forms.Form):
