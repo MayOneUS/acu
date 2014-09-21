b4718db5b47c5751ec456331568ba839446252b3
Jacob A. Brennan <jacobabrennan@gmail.com>
2014-09-21 11:31:59 -0400

    Basic Flow Complete, starting linking to backend
    HTML structure for basic user interaction complete.

--- rsc/script/vid_quiz.js
+++ rsc/script/vid_quiz.js
@@ -1,54 +1,67 @@
 var entryCodeURI = '/entry_code';
-var quiz_question ={
+var quizQuestion ={
 	description: 'Query',
 	answers:  []
 };
 var quiz = {
-	question_index: 0,
+	questionIndex: 0,
 	questions: [
-		Object.create(quiz_question, {
+		Object.create(quizQuestion, {
+			id: {value: 0},
 			description: {value: 'This is Question 1'},
 			answers: {value: ['Answer1', 'Answer2', 'Kittens']}
 		}),
-		Object.create(quiz_question, {
+		Object.create(quizQuestion, {
+			id: {value: 1},
 			description: {value: 'This is Question 2'},
 			answers: {value: ['Answer1', 'Answer2', 'Kittens']}
 		}),
-		Object.create(quiz_question, {
+		Object.create(quizQuestion, {
+			id: {value: 2},
 			description: {value: 'This is Kittens!'},
 			answers: {value: ['Kittens', 'Kittens!', 'More Kittens!']}
 		})
 	],
-	next_question: function (){
-		var next_q = this.questions[this.question_index];
-		this.question_index++;
-		if(!next_q){
-			validateQuiz();
-		}
-		return next_q;
+	nextQuestion: function (){
+		var nextQ = this.questions[this.questionIndex];
+		this.questionIndex++;
+		return nextQ;
 	}
 };
-function load_question(question){
-	var question_container = document.getElementById('quiz_question_display');
-	question_container.innerHTML = "";
-	var question_description = document.createElement('span');
-	question_description.textContent = question.description;
-	console.log(question.description)
-	question_container.appendChild(question_description);
-	question.answers.forEach(function (answer){
-		var answer_button = document.createElement('button');
-		answer_button.textContent = answer;
-		question_container.appendChild(answer_button);
-		answer_button.addEventListener("click", function(){
-			answer_question(answer);
+function loadQuestion(question){
+	var questionContainer = document.getElementById('quiz_question_display');
+	jContainer = jQuery('#quiz_question_display');
+	jContainer.fadeOut(300, function (){
+		questionContainer.innerHTML = "";
+		var questionDescription = document.createElement('span');
+		questionDescription.textContent = question.description;
+		questionContainer.appendChild(questionDescription);
+		question.answers.forEach(function (answer){
+			var answerButton = document.createElement('button');
+			answerButton.textContent = answer;
+			questionContainer.appendChild(answerButton);
+			answerButton.addEventListener("click", function(){
+				answerQuestion(question.id, answer);
+			});
 		});
+		jContainer.fadeIn(300);
 	});
 };
-function answer_question(answer){
-	load_question(quiz.next_question());
+function answerQuestion(questionId, answer){
+	var quizForm = document.getElementById('quiz_submitter');
+	var answerInput = document.createElement('input');
+	answerInput.name = questionId;
+	answerInput.value = answer;
+	var nextQuestion = quiz.nextQuestion();
+	quizForm.appendChild(answerInput);
+	if(!nextQuestion){
+		finishQuiz();
+	} else{
+		loadQuestion(nextQuestion);
+	}
 };
 
-function submitEntryCode(entryCode) {
+function submitQuiz() {
 	/*jQuery.ajax({
 		url: entryCodeURI,
 		data: entryCode,
@@ -57,27 +70,31 @@ function submitEntryCode(entryCode) {
 		complete: function (jqXHR, textStatus){},
 	});
 	*/
-	setTimeout(function (){
-		jQuery('#step_code').fadeOut(500, function (){
-			jQuery('#step_results').fadeIn(500);
-		});
-	}, 0);
+	// TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	var redemptionCodeInput = document.getElementById('redemption_code_input');
+	var redemptionCode = redemptionCodeInput.value;
+	var quizForm = document.getElementById('quiz_submitter');
+	var codeInput = document.createElement('input');
+	codeInput.name = 'redemption_code';
+	codeInput.value = redemptionCode;
+	quizForm.appendChild(codeInput);
+	quizForm.submit()
+	return false
 };
 function videoComplete() {
 	setTimeout(function (){
-		var question = quiz.next_question();
-		load_question(question);
+		var question = quiz.nextQuestion();
+		loadQuestion(question);
 		jQuery('#step_video').fadeOut(500, function (){
 			jQuery('#step_quiz').fadeIn(500);
 		});
 	}, 0);
 };
-function validateQuiz() {
+function finishQuiz() {
 	setTimeout(function (){
 		jQuery('#step_quiz').fadeOut(500, function (){
 			jQuery('#step_code').fadeIn(500);
 		});
 	}, 0);
 };
-
-jQuery('#redemption_code').submit(submitEntryCode);
+jQuery('#redemption_code').submit(submitQuiz);
