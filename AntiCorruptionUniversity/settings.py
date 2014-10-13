fffddc44747a7965bf8a307b7258f54957589654
daniel <danielbporter@gmail.com>
2014-10-13 11:19:12 -0400

    fixing settings file

--- AntiCorruptionUniversity/settings.py
+++ AntiCorruptionUniversity/settings.py
@@ -89,6 +89,7 @@ STATICFILES_DIRS = (
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
+DATABASES['default'] =  dj_database_url.config()
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
