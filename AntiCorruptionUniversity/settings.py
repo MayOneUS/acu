f06defeea25946aedbcbdda28672db4cdd0966f1
sbwinter <swinter2011@gmail.com>
2014-10-17 16:57:17 -0600

    Update Card Page
    update card page and copy edits

--- AntiCorruptionUniversity/settings.py
+++ AntiCorruptionUniversity/settings.py
@@ -89,7 +89,6 @@ STATICFILES_DIRS = (
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
-DATABASES['default'] =  dj_database_url.config()
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
